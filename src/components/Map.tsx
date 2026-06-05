48c1d868296b8f4ff433fd11bc2367f34db8f0dd
TRISTAN <tristan@SPY.kali>
2026-06-05 04:41:17 -0700

    feat: add click marker and smooth fly-to on map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,15 +1,30 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
+import { useEffect, useState, useMemo, useRef } from 'react';
+import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+// ---------- fix default icon path issue (common with bundlers) ----------
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({ iconUrl, iconRetinaUrl, shadowUrl });
+
+// ---------- types ----------
 export interface MapLocation {
   lat: number;
   lon: number;
 }
 
-function LocationMarker({ onLocationSelect }: { onLocationSelect: (lat: number, lon: number) => void }) {
+// ---------- inner components ----------
+function LocationMarker({
+  onLocationSelect,
+}: {
+  onLocationSelect: (lat: number, lon: number) => void;
+}) {
   useMapEvents({
     click(e) {
       onLocationSelect(e.latlng.lat, e.latlng.lng);
@@ -18,6 +33,18 @@ function LocationMarker({ onLocationSelect }: { onLocationSelect: (lat: number, 
   return null;
 }
 
+/** Pans the map to the selected location whenever it changes */
+function FlyToSelected({ loc }: { loc: MapLocation | null }) {
+  const map = useMap();
+  useEffect(() => {
+    if (loc) {
+      map.flyTo([loc.lat, loc.lon], map.getZoom(), { duration: 0.8 });
+    }
+  }, [loc, map]);
+  return null;
+}
+
+// ---------- main component ----------
 export default function Map({
   selectedLocation,
   onLocationSelect,
@@ -41,7 +68,7 @@ export default function Map({
 
   return (
     <MapContainer
-      center={[selectedLocation?.lat ?? -1.2921, selectedLocation?.lon ?? 36.8219]}
+      center={[-1.2921, 36.8219]}
       zoom={6}
       scrollWheelZoom={true}
       className="h-[400px] w-full rounded-xl shadow-md z-0"
@@ -51,6 +78,8 @@ export default function Map({
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <LocationMarker onLocationSelect={onLocationSelect} />
+      {selectedLocation && <Marker position={[selectedLocation.lat, selectedLocation.lon]} />}
+      <FlyToSelected loc={selectedLocation} />
     </MapContainer>
   );
 }
